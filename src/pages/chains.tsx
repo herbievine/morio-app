79bbb31955c3d84764b2d2f243b45bff6b48b0b8
Herbie Vine <vineherbie@gmail.com>
2022-01-28 22:39:01 +0000

    hopefully fixed env issue

--- src/pages/chains.tsx
+++ src/pages/chains.tsx
@@ -15,7 +15,8 @@ const Chains: NextPage<ChainsProps> = () => {
           <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 auto-rows-auto gap-5">
             {networks
               .filter(
-                (network) => process.env.VERCEL_ENV === network.environment
+                ({ environment }) =>
+                  process.env.NEXT_PUBLIC_VERCEL_ENV === environment
               )
               .sort((a, b) => (a.currency < b.currency ? -1 : 1))
               .map((network) => (
