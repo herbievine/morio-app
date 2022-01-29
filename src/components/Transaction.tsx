62148c1e000ad1856d8d955a7e4a758177db2cb6
Herbie Vine <vineherbie@gmail.com>
2022-01-29 17:09:43 +0000

    changed addresses the reflect the new contracts on test networks

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -14,7 +14,7 @@ const Transaction: React.FC<TransactionProps> = ({
   const { network } = useNetwork();
 
   return (
-    <div className="p-6 w-10/12 sm:w-3/4 md:w-1/2 lg:w-1/4 rounded-md bg-neutral-800">
+    <div className="p-6 w-5/6 md:w-1/2 lg:w-1/3 xl:1/4 rounded-md bg-neutral-800">
       <div className="flex items-center justify-between">
         <p className="font-bold text-neutral-300">
           Transaction submitted successfully
@@ -31,7 +31,12 @@ const Transaction: React.FC<TransactionProps> = ({
         </p>
       </div>
       <div className="mt-6 p-6 flex flex-col items-center rounded-md bg-neutral-900 text-neutral-300 font-bold">
-        <a className="w-full mb-2" href={network.explorer + "tx/" + hash}>
+        <a
+          className="w-full mb-2"
+          href={network.explorer + "tx/" + hash}
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className="w-full flex justify-between items-center">
             <p className="pr-6">Hash</p>
             <p className="truncate">{hash.slice(0, 20)}...</p>
@@ -40,13 +45,20 @@ const Transaction: React.FC<TransactionProps> = ({
         <a
           className="w-full mb-2"
           href={network.explorer + "address/" + to + "?fromaddress=" + from}
+          target="_blank"
+          rel="noreferrer"
         >
           <div className="w-full flex justify-between items-center">
             <p className="pr-6">To</p>
             <p className="truncate">{to.slice(0, 20)}...</p>
           </div>
         </a>
-        <a className="w-full" href={network.explorer + "address/" + from}>
+        <a
+          className="w-full"
+          href={network.explorer + "address/" + from}
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className="w-full flex justify-between items-center">
             <p className="pr-6">From</p>
             <p className="truncate">{from.slice(0, 20)}...</p>
